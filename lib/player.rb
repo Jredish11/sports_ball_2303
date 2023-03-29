a083ecc88c3405a92bad439b315e8c2701c2156f
jeffreyredish <jeffreyredish@Jeffreys-MacBook-Pro>
2023-03-29 09:45:25 -0600

    feat: Add methods for total_cost and set nickname

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,24 @@
 class Player
-  attr_reader :name, :monthly_cost, :contract_length, :first_name, :last_name
+  attr_reader :name, 
+              :monthly_cost, 
+              :contract_length, 
+              :first_name, 
+              :last_name,
+              :nickname
   def initialize(name, monthly_cost, contract_length)
     @name = name
     @monthly_cost = monthly_cost
     @contract_length = contract_length 
     @first_name = name.split.first
     @last_name = name.split.last
+    @nickname = nil
   end
 
   def total_cost
     @monthly_cost * @contract_length
+  end
+
+  def set_nickname!(nickname)
+    @nickname = nickname
   end
 end
